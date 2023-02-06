8d92b5cc6cbec437025cdcf2e641f01fee6d8b3e
Nishanth Kumar Pathi <pathinishanth@gmail.com>
2023-02-06 23:59:30 +0300

    public, app, db, efs sg structure created

---
+++ 11-sg-outputs.tf
@@ -0,0 +1,44 @@
+# Outputs of the Security Group
+
+## Public Services Security Group
+output "public_services_sg_id" {
+    value = module.public_services_sg.security_group_id
+}
+
+output "public_services_sg_arn" {
+    value = module.public_services_sg.security_group_arn
+}
+
+output "public_services_sg_name" {
+    value = module.public_services_sg.security_group_name
+}
+
+output "public_services_sg_description" {
+    value = module.public_services_sg.security_group_description
+}
+
+
+# Application Services Security Group
+output "app_services_sg_id" {
+    value = module.app_services_sg.security_group_id
+}
+
+output "app_services_sg_arn" {
+    value = module.app_services_sg.security_group_arn
+}
+
+
+
+
+# Database Services Security Group
+output "db_services_sg_id" {
+    value = module.db_services_sg.this_security_group_id
+}
+
+
+# EFS Services Security Group
+output "efs_services_sg_id" {
+    value = module.efs_services_sg.this_security_group_id
+}
+
+
